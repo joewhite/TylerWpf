b648fca81eadc275072fc5303157be49c257ab90
Joe White <git@excastle.com>
2009-06-02 07:30:42 -0500

    Move the hero around with the keyboard.

--- Tyler/MapView.xaml.cs
+++ Tyler/MapView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Tyler
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Tyler
 {
     /// <summary>
     /// Interaction logic for MapView.xaml
@@ -8,6 +11,34 @@
         public MapView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    Canvas.SetLeft(hero, Canvas.GetLeft(hero) - 16);
+                    e.Handled = true;
+                    break;
+                case Key.Right:
+                    Canvas.SetLeft(hero, Canvas.GetLeft(hero) + 16);
+                    e.Handled = true;
+                    break;
+                case Key.Up:
+                    Canvas.SetTop(hero, Canvas.GetTop(hero) - 16);
+                    e.Handled = true;
+                    break;
+                case Key.Down:
+                    Canvas.SetTop(hero, Canvas.GetTop(hero) + 16);
+                    e.Handled = true;
+                    break;
+            }
+        }
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Focus();
+            e.Handled = true;
         }
     }
 }
