9b15d4390e364184f735f635701210fa428ea023
Joe White <git@excastle.com>
2009-06-02 07:01:27 -0500

    Added MapView.

---
+++ Tyler/MapView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyler
+{
+    /// <summary>
+    /// Interaction logic for MapView.xaml
+    /// </summary>
+    public partial class MapView
+    {
+        public MapView()
+        {
+            InitializeComponent();
+        }
+    }
+}
