1308dbe96c15d35db69c96349ac4751e27d92108
Joe White <git@excastle.com>
2009-06-02 06:59:16 -0500

    New project.

---
+++ Tyler/GameWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyler
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
