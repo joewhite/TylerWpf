8a62535551538fc713dbd1383d95f8ff01ff7c4b
Joe White <git@excastle.com>
2009-06-02 06:59:32 -0500

    Added HeroView.

---
+++ Tyler/HeroView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyler
+{
+    /// <summary>
+    /// Interaction logic for HeroView.xaml
+    /// </summary>
+    public partial class HeroView
+    {
+        public HeroView()
+        {
+            InitializeComponent();
+        }
+    }
+}
