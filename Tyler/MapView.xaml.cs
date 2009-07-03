c1b2576857392c8b84cb9b002466a33634fb9d12
Joe White <git@excastle.com>
2009-07-03 14:09:20 -0500

    Extracted a Walk method.

--- Tyler/MapView.xaml.cs
+++ Tyler/MapView.xaml.cs
@@ -22,43 +22,42 @@ namespace Tyler
         }
         private void UserControl_KeyDown(object sender, KeyEventArgs e)
         {
-            var oldLeft = Canvas.GetLeft(hero);
-            var oldTop = Canvas.GetTop(hero);
-            var newLeft = oldLeft;
-            var newTop = oldTop;
+            e.Handled = true;
             switch (e.Key)
             {
                 case Key.Left:
-                    newLeft = oldLeft - 1;
-                    hero.Facing = Facing.West;
+                    Walk(Facing.West, -1, 0);
                     break;
                 case Key.Right:
-                    newLeft = oldLeft + 1;
-                    hero.Facing = Facing.East;
+                    Walk(Facing.East, 1, 0);
                     break;
                 case Key.Up:
-                    newTop = oldTop - 1;
-                    hero.Facing = Facing.North;
+                    Walk(Facing.North, 0, -1);
                     break;
                 case Key.Down:
-                    newTop = oldTop + 1;
-                    hero.Facing = Facing.South;
+                    Walk(Facing.South, 0, 1);
                     break;
+                default:
+                    e.Handled = false;
+                    return;
             }
-            if (newLeft == oldLeft && newTop == oldTop)
-                return;
+        }
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Focus();
             e.Handled = true;
+        }
+        private void Walk(Facing facing, int deltaX, int deltaY)
+        {
+            hero.Facing = facing;
+            var newLeft = Canvas.GetLeft(hero) + deltaX;
+            var newTop = Canvas.GetTop(hero) + deltaY;
             if (!IsPassable((int) newLeft, (int) newTop))
                 return;
             Canvas.SetLeft(hero, newLeft);
             Canvas.SetTop(hero, newTop);
             Canvas.SetLeft(scroller, -newLeft);
             Canvas.SetTop(scroller, -newTop);
-        }
-        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Focus();
-            e.Handled = true;
         }
     }
 }
