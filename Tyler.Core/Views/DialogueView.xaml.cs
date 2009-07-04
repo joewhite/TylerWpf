ccd49b8cafbb3217b0a0b12df19164c7536c9a27
Joe White <git@excastle.com>
2009-07-04 01:07:08 -0500

    Added dialogue view. Currently always shown.

---
+++ Tyler.Core/Views/DialogueView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyler.Views
+{
+    /// <summary>
+    /// Interaction logic for DialogueView.xaml
+    /// </summary>
+    public partial class DialogueView
+    {
+        public DialogueView()
+        {
+            InitializeComponent();
+        }
+    }
+}
