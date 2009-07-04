2e2c5926b46acc379b5840e74cab6fd96613428b
Joe White <git@excastle.com>
2009-07-04 01:39:23 -0500

    Show/hide the "Back", "Next", and "Close" flair in the dialogue window.

---
+++ Tyler.Core/Views/VisibleWhenValueConverter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tyler.Views
+{
+    public class VisibleWhenValueConverter : IValueConverter
+    {
+        public bool Value { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(value, Value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
