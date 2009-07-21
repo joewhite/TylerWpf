e3f86b93211821ac7096e05a35822b53efc3b726
Joe White <git@excastle.com>
2009-07-21 07:56:09 -0500

    Add Autofac and use it to instantiate GameWindow.

--- Tyler/App.xaml.cs
+++ Tyler/App.xaml.cs
@@ -1,9 +1,22 @@
-﻿namespace Tyler
+﻿using System.Windows;
+using Autofac.Builder;
+
+namespace Tyler
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterTypesAssignableTo<Window>().FactoryScoped();
+            using (var container = builder.Build())
+            {
+                var gameWindow = container.Resolve<GameWindow>();
+                gameWindow.Show();
+            }
+        }
     }
 }
